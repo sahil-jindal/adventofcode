da2de1efed93a734e57bcc580712e5d4d9741f86
Sahil Jindal <sahiljindal.thebest@gmail.com>
2024-12-27 16:17:02 +0000

    Skipping day 6 part 2 for day 7

--- 2024/src/main/scala/daysix/day6.scala
+++ 2024/src/main/scala/daysix/day6.scala
@@ -3,6 +3,7 @@ package daysix
 import scala.util.{Try, Success, Failure}
 import scala.io.Source
 import scala.collection.mutable.Set
+import scala.collection.mutable.ArrayBuffer
 
 // Constants
 val EMPTY_CELL = '.'
@@ -73,12 +74,40 @@ def getMovementsMatrix(_matrix: Array[Array[Char]]): Array[Array[Boolean]] =
 
     visitedMatrix
 
-def getDistinctPositions(matrix: Array[Array[Boolean]]): Int =
-    matrix.map(_.count(identity)).sum
+def checkLoop(matrix: Array[Array[Char]], obstacleX: Int, obstacleY: Int): Boolean =
+    val clonedMatrix = matrix.map(_.clone)
+    clonedMatrix(obstacleX)(obstacleY) = OBSTACLE_CELL
+    val visitedMatrix = clonedMatrix.map(_.map(_ == START_CELL))
 
-def getSolution(matrix: Array[Array[Char]]): Int =
-    val movementsMatrix = getMovementsMatrix(matrix)
-    getDistinctPositions(movementsMatrix)
+    val startPosition = findPosition(clonedMatrix).getOrElse(Position(0, 0))
+    var currentDirection = Direction.Up
+
+    val sequence = ArrayBuffer((startPosition, currentDirection))
+
+    while true do
+        val (outOfBounds, newDirection) = step(clonedMatrix, visitedMatrix, currentDirection)
+        if outOfBounds then return false
+        currentDirection = newDirection
+
+        findPosition(clonedMatrix) match
+            case Some(pos) =>
+                if sequence.contains((pos, currentDirection)) then return true
+                sequence.append((pos, currentDirection))
+            case None => return false
+
+def getCoordinates(matrix: Array[Array[Char]], visitedMatrix: Array[Array[Boolean]]): ArrayBuffer[Position] = 
+    val coordinates = ArrayBuffer[Position]()
+    
+    for (i <- visitedMatrix.indices; j <- visitedMatrix(i).indices if visitedMatrix(i)(j)) {
+        val startPos = findPosition(matrix)
+        if (!startPos.contains(Position(i, j)))
+            coordinates.append(Position(i, j))
+    }
+    
+    coordinates
+
+def evalutorOne(matrix: Array[Array[Char]]): Int =
+    getMovementsMatrix(matrix).map(_.count(identity)).sum
 
 // def evalutorTwo(manuals: List[Manual]): Unit =
 //     val previouslyInorderlyManuals = manuals.filterNot(isOrdered)
@@ -106,7 +135,7 @@ def hello(): Unit =
     readLinesFromFile("src/main/scala/daysix/file.txt") match
         case Success(lines) => {  
             val input = lines.map(_.toCharArray).toArray      
-            val result = getSolution(input)
+            val result = evalutorOne(input)
             println(s"Distinct positions visited: $result")
         }
         case Failure(exception) => {
