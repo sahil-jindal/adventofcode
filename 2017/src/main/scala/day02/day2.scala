3006110480cc006fa3b92ec03862efb734cb74e4
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-02-02 16:20:26 +0000

    Refactored 2017 Day 2

--- 2017/src/main/scala/day02/day2.scala
+++ 2017/src/main/scala/day02/day2.scala
@@ -1,25 +1,30 @@
-package day01
+package day02
 
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
 
-def solve(line: Array[Int], rotated: Int) = {
-    val d = rotated % line.length
-    val leftRotated = line.drop(d) ++ line.take(d)
-    val newLine = line zip leftRotated
-    newLine.collect { case (a, b) if a == b => a }.sum
+def parseInput(lines: List[String]) = {
+    lines.map("(\\d+)".r.findAllIn(_).toArray.map(_.toInt)).toArray
 }
 
-def evaluatorOne(line: Array[Int]) = solve(line, 1)
-def evaluatorTwo(line: Array[Int]) = solve(line, line.length / 2)
+def evaluatorOne(input: Array[Array[Int]]) = {
+    input.map { it => it.max - it.min }.sum
+}
+
+def evaluatorTwo(input: Array[Array[Int]]) = {
+    input.flatMap { numbers =>
+        for { a <- numbers; b <- numbers; if a > b && a % b == 0 } yield a / b
+    }.sum
+}
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
     Using(Source.fromResource(filePath))(_.getLines().toList)
 
+@main
 def hello(): Unit =
-    readLinesFromFile("day1.txt") match
+    readLinesFromFile("day2.txt") match
         case Success(lines) => {
-            val input = lines(0).map(_.asDigit).toArray
+            val input = parseInput(lines)
             println(s"Part One: ${evaluatorOne(input)}")
             println(s"Part Two: ${evaluatorTwo(input)}")
         }
