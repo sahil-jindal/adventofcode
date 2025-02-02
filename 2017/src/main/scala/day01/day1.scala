6636b7188e5999fe88a9fdf0062c1d376e02c342
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-02-02 14:18:49 +0000

    Finished 2017 Day 1

---
+++ 2017/src/main/scala/day01/day1.scala
@@ -0,0 +1,29 @@
+package day01
+
+import scala.util.{Try, Success, Failure, Using}
+import scala.io.Source
+
+def solve(line: Array[Int], rotated: Int) = {
+    val d = rotated % line.length
+    val temp = line.drop(d) ++ line.take(d)
+    val newLine = line zip temp
+    newLine.collect { case (a, b) if a == b => a }.sum
+}
+
+def evaluatorOne(line: Array[Int]) = solve(line, 1)
+def evaluatorTwo(line: Array[Int]) = solve(line, line.length / 2)
+
+def readLinesFromFile(filePath: String): Try[List[String]] =
+    Using(Source.fromResource(filePath))(_.getLines().toList)
+
+@main
+def hello(): Unit =
+    readLinesFromFile("day1.txt") match
+        case Success(lines) => {
+            val input = lines(0).map(_.asDigit).toArray
+            println(s"Part One: ${evaluatorOne(input)}")
+            println(s"Part Two: ${evaluatorTwo(input)}")
+        }
+        case Failure(exception) => {
+            println(s"Error reading file: ${exception.getMessage}")
+        }
