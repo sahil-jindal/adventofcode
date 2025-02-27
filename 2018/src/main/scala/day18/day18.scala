c3bbd4320ed7735dc4e6013b9573ea8fa68fc7a8
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-02-27 19:41:57 +0000

    Finished 2018 Day 18

---
+++ 2018/src/main/scala/day18/day18.scala
@@ -0,0 +1,91 @@
+package day18
+
+import scala.util.{Try, Success, Failure, Using}
+import scala.io.Source
+import scala.collection.mutable.{ListBuffer, Map}
+
+def step(mtx: List[String]): List[String] = {
+    val res = ListBuffer.empty[String]
+
+    val crow = mtx.length
+    val ccol = mtx(0).length
+  
+    for (irow <- 0 until crow) {
+        var line = ""
+        for (icol <- 0 until ccol) {
+            var (tree, lumberyard, empty) = (0, 0, 0)
+      
+            for (drow <- Seq(-1, 0, 1); dcol <- Seq(-1, 0, 1) if drow != 0 || dcol != 0) {
+                val (icolT, irowT) = (icol + dcol, irow + drow)
+                if (icolT >= 0 && icolT < ccol && irowT >= 0 && irowT < crow) {
+                    mtx(irowT)(icolT) match {
+                        case '#' => lumberyard += 1
+                        case '|' => tree += 1
+                        case '.' => empty += 1
+                        case _   =>
+                    }
+                }
+            }
+
+            line += (mtx(irow)(icol) match {
+                case '#' if lumberyard >= 1 && tree >= 1 => '#'
+                case '|' if lumberyard >= 3 => '#'
+                case '.' if tree >= 3 => '|'
+                case '#' => '.'
+                case c => c
+            })
+        }
+        
+        res.append(line)
+    }
+
+    return res.toList
+}
+
+def iterate(input: List[String], lim: Int): Int = {
+    val seen = Map.empty[String, Int]
+    var mtx = input
+    
+    var t = 0
+    
+    while (t < lim) {
+        val hash = mtx.mkString("")
+        
+        if (seen.contains(hash)) {
+            val loopLength = t - seen(hash)
+            val remainingSteps = lim - t - 1
+            val remainingLoops = remainingSteps / loopLength
+            t += remainingLoops * loopLength
+        } else {
+            seen(hash) = t
+        }
+        
+        mtx = step(mtx)
+        t += 1
+    }
+
+    val res = mtx.mkString("")
+
+    val treeCount = "\\|".r.findAllIn(res).length
+    val lumberyardCount = "#".r.findAllIn(res).length
+
+    treeCount * lumberyardCount
+}
+
+def evaluatorOne(input: List[String]): Int = iterate(input, 10)
+def evaluatorTwo(input: List[String]): Int = iterate(input, 1000000000)
+
+def readLinesFromFile(filePath: String): Try[List[String]] =
+    Using(Source.fromResource(filePath))(_.getLines().toList)
+
+def hello(): Unit = {
+    readLinesFromFile("day18.txt") match {
+        case Success(lines) => {
+            println(s"Part One: ${evaluatorOne(lines)}")
+            println(s"Part Two: ${evaluatorTwo(lines)}")
+        }
+        case Failure(exception) => {
+            println(s"Error reading file: ${exception.getMessage}")
+        }
+    }
+}
