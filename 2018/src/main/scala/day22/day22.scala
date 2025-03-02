27dd88acd3afbf1d936f8a05b549ae5b6fc3ef9e
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-03-02 16:12:55 +0000

    Finished 2018 Day 22

---
+++ 2018/src/main/scala/day22/day22.scala
@@ -0,0 +1,93 @@
+package day22
+
+import scala.util.{Try, Success, Failure, Using}
+import scala.io.Source
+import scala.collection.mutable.{Map, PriorityQueue, Set}
+
+case class Point(x: Int, y: Int)
+
+enum RegionType { case Rocky, Wet, Narrow }
+
+enum Tool { case Nothing, Torch, ClimbingGear }
+
+def parse(lines: List[String]): (Point, Point => RegionType) = {
+    val depth = "\\d+".r.findAllIn(lines(0)).mkString.toInt
+    val Array(targetX, targetY) = "\\d+".r.findAllIn(lines(1)).map(_.toInt).toArray
+    val erosionLevelCache = Map.empty[Point, Int]
+    val modulo = 20183
+
+    def erosionLevel(p: Point): Int = erosionLevelCache.getOrElseUpdate(p, {
+        if (p == Point(0, 0) || p == Point(targetX, targetY)) depth % modulo
+        else if (p.x == 0) ((p.y * 48271) + depth) % modulo
+        else if (p.y == 0) ((p.x * 16807) + depth) % modulo
+        else ((erosionLevel(Point(p.x - 1, p.y)) * erosionLevel(Point(p.x, p.y - 1))) + depth) % modulo
+    })
+    
+    def regionType(p: Point): RegionType = RegionType.fromOrdinal(erosionLevel(p) % 3)
+    
+    return (Point(targetX, targetY), regionType)
+}
+
+def partOne(input: List[String]): Int = {
+    val (target, regionType) = parse(input)
+    val result = for { y <- 0 to target.y; x <- 0 to target.x } yield regionType(Point(x, y)).ordinal
+    return result.sum
+}
+
+def partTwo(input: List[String]): Int = {
+    val (target, regionType) = parse(input)
+    
+    def neighbours(pos: Point, tool: Tool): Seq[(Point, Tool, Int)] = {
+        val switchTool = regionType(pos) match {
+            case RegionType.Rocky  => if tool == Tool.ClimbingGear then Tool.Torch else Tool.ClimbingGear
+            case RegionType.Narrow => if tool == Tool.Torch then Tool.Torch else Tool.Nothing
+            case RegionType.Wet    => if tool == Tool.ClimbingGear then Tool.Nothing else Tool.ClimbingGear
+        }
+    
+        val moveDirs = Seq(Point(-1, 0), Point(1, 0), Point(0, -1), Point(0, 1))
+
+        val moves = moveDirs.map(d => Point(pos.x + d.x, pos.y + d.y))
+            .filter(p => p.x >= 0 && p.y >= 0).filter{p =>
+                val rt = regionType(p)
+                (rt == RegionType.Rocky && (tool == Tool.ClimbingGear || tool == Tool.Torch)) ||
+                (rt == RegionType.Narrow && (tool == Tool.Torch || tool == Tool.Nothing)) ||
+                (rt == RegionType.Wet && (tool == Tool.ClimbingGear || tool == Tool.Nothing))
+            }.map((_, tool, 1))
+    
+        return moves :+ (pos, switchTool, 7)
+    }
+    
+    val q = PriorityQueue.empty(Ordering.by[(Int, Point, Tool, Int), Int](-_._1))
+    val seen = Set.empty[(Point, Tool)]
+
+    q.enqueue((0, Point(0, 0), Tool.Torch, 0))
+
+    while (q.nonEmpty) {
+        val (_, pos, tool, t) = q.dequeue()
+        if (pos == target && tool == Tool.Torch) return t
+        if (!seen((pos, tool))) {
+            seen += ((pos, tool))
+            for ((newPos, newTool, dt) <- neighbours(pos, tool)) {
+                val priority = t + dt + (target.x - newPos.x).abs + (target.y - newPos.y).abs
+                q.enqueue((priority, newPos, newTool, t + dt))
+            }
+        }
+    }
+    
+    throw new Exception("No path found")
+}
+
+def readLinesFromFile(filePath: String): Try[List[String]] =
+    Using(Source.fromResource(filePath))(_.getLines().toList)
+
+def hello(): Unit = {
+    readLinesFromFile("day22.txt") match {
+        case Success(lines) => {
+            println(s"Part One: ${partOne(lines)}")
+            println(s"Part Two: ${partTwo(lines)}")
+        }
+        case Failure(exception) => {
+            println(s"Error reading file: ${exception.getMessage}")
+        }
+    }
+}
