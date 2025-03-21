2e2ac7aca71ccdf4783cfbf7f370b63a90a4cbb9
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-03-21 09:10:10 +0000

    Redone 2019 Day 8

---
+++ 2019/src/main/scala/day08/day8.scala
@@ -0,0 +1,44 @@
+package day08
+
+import scala.util.{Try, Success, Failure, Using}
+import scala.io.Source
+
+def parseInput(input: String): List[Seq[Int]] = input.map(_.asDigit).grouped(6 * 25).toList
+
+def evaluatorOne(layers: List[Seq[Int]]): Int = {
+    val layer = layers.minBy(_.count(_ == 0))
+    val ones = layer.count(_ == 1)
+    val twos = layer.count(_ == 2)
+    return ones * twos
+}
+
+def evaluatorTwo(layers: List[Seq[Int]]): String = {
+    val img = Array.fill(6 * 25)(' ')
+
+    for (layer <- layers.reverse; i <- img.indices) {
+        img(i) = layer(i) match {
+            case 0 => ' '
+            case 1 => '#'
+            case _ => img(i)
+        }
+    }
+
+    return img.grouped(25).map(_.mkString).mkString("\n")
+}
+
+def readLinesFromFile(filePath: String): Try[List[String]] =
+    Using(Source.fromResource(filePath))(_.getLines().toList)
+
+@main
+def hello(): Unit = {
+    readLinesFromFile("day08.txt") match {
+        case Success(lines) => {
+            val layers = parseInput(lines.head)
+            println(s"Part One: ${evaluatorOne(layers)}")
+            println(s"Part Two:\n${evaluatorTwo(layers)}")
+        }
+        case Failure(exception) => {
+            println(s"Error reading file: ${exception.getMessage}")
+        }
+    }
+}
