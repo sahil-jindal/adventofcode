73a228a2951cee07195c4ad05800c940cdb8c080
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-03-26 13:02:20 +0000

    Finished 2020 Day 5

---
+++ 2020/src/main/scala/day05/day5.scala
@@ -0,0 +1,32 @@
+package day05
+
+import scala.util.{Try, Success, Failure, Using}
+import scala.io.Source
+
+def parseInput(input: List[String]): Set[Int] = input.map(line => {
+    val binaryString = line.replace('B', '1').replace('F', '0').replace('R', '1').replace('L', '0')
+    Integer.parseInt(binaryString, 2)
+}).toSet
+
+def evaluatorOne(seats: Set[Int]): Int = seats.max
+
+def evaluatorTwo(seats: Set[Int]): Int = {
+    val (min, max) = (seats.min, seats.max)
+    return (min to max).find(id => !seats.contains(id)).get
+}
+
+def readLinesFromFile(filePath: String): Try[List[String]] =
+    Using(Source.fromResource(filePath))(_.getLines().toList)
+
+def hello(): Unit = {
+    readLinesFromFile("day05.txt") match {
+        case Success(lines) => {
+            val input = parseInput(lines)
+            println(s"Part One: ${evaluatorOne(input)}")
+            println(s"Part Two: ${evaluatorTwo(input)}")
+        }
+        case Failure(exception) => {
+            println(s"Error reading file: ${exception.getMessage}")
+        }
+    }
+}
