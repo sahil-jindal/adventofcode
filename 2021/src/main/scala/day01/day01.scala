5c4bdae498b1f06db3809434478576feec28a448
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-04-04 11:02:41 +0000

    Finished 2021 Day 1

---
+++ 2021/src/main/scala/day01/day01.scala
@@ -0,0 +1,27 @@
+package day01
+
+import scala.util.{Try, Success, Failure, Using}
+import scala.io.Source
+
+def parseInput(input: List[String]): List[Int] = input.map(_.toInt)
+def depthIncrease(ns: List[Int]): Int = (ns.init zip ns.tail).count { case (a, b) => a < b }
+def threeMeasurements(ns: List[Int]): List[Int] = ns.sliding(3).map(_.sum).toList
+
+def evaluatorOne(ns: List[Int]) = depthIncrease(ns)
+def evaluatorTwo(ns: List[Int]) = depthIncrease(threeMeasurements(ns))
+
+def readLinesFromFile(filePath: String): Try[List[String]] =
+    Using(Source.fromResource(filePath))(_.getLines().toList)
+
+def hello(): Unit = {
+    readLinesFromFile("day01.txt") match {
+        case Success(lines) => {
+            val input = parseInput(lines)
+            println(s"Part One: ${evaluatorOne(input)}")
+            println(s"Part Two: ${evaluatorTwo(input)}")
+        }
+        case Failure(exception) => {
+            println(s"Error reading file: ${exception.getMessage}")
+        }
+    }
+}
