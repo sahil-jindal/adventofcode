9546153ecfff460d88e0331c7a56c7784c060de6
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-04-05 14:10:09 +0000

    Finished 2021 Day 7

---
+++ 2021/src/main/scala/day07/day7.scala
@@ -0,0 +1,31 @@
+package day07
+
+import scala.util.{Try, Success, Failure, Using}
+import scala.io.Source
+
+def parseInput(input: String): List[Int] = input.split(",").map(_.toInt).toList
+
+def fuelMin(positions: List[Int], fuelConsumption: Int => Int): Int = {
+    return (positions.min to positions.max).map(target => {
+        positions.map(it => fuelConsumption(math.abs(target - it))).sum
+    }).min
+}
+
+def evaluatorOne(positions: List[Int]): Int = fuelMin(positions, identity)
+def evaluatorTwo(positions: List[Int]): Int = fuelMin(positions, dist => dist * (dist + 1) / 2)
+
+def readLinesFromFile(filePath: String): Try[List[String]] =
+    Using(Source.fromResource(filePath))(_.getLines().toList)
+
+def hello(): Unit = {
+    readLinesFromFile("day07.txt") match {
+        case Success(lines) => {
+            val input = parseInput(lines.head)
+            println(s"Part One: ${evaluatorOne(input)}")
+            println(s"Part Two: ${evaluatorTwo(input)}")
+        }
+        case Failure(exception) => {
+            println(s"Error reading file: ${exception.getMessage}")
+        }
+    }
+}
