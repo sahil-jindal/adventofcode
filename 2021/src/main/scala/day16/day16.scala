4d05c089060605b30150e2267fde55715b559b90
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-04-07 10:55:07 +0000

    Finished 2021 Day 16

---
+++ 2021/src/main/scala/day16/day16.scala
@@ -0,0 +1,97 @@
+package day16
+
+import scala.util.{Try, Success, Failure, Using}
+import scala.io.Source
+import scala.collection.mutable.ListBuffer
+
+case class Packet(version: Int, `type`: Int, payload: Long, packets: List[Packet])
+
+case class BitSequenceReader(private val bits: Seq[Boolean]) {
+    private var ptr = 0
+
+    def nonEmpty: Boolean = ptr < bits.length
+
+    def getBitSequenceReader(bitCount: Int): BitSequenceReader = {
+        val subBits = bits.slice(ptr, ptr + bitCount)
+        ptr += bitCount
+        return BitSequenceReader(subBits)
+    }
+
+    def readInt(bitCount: Int): Int = {
+        val subBits = bits.slice(ptr, ptr + bitCount).map(if _ then 1 else 0).mkString("")
+        ptr += bitCount
+        return Integer.parseInt(subBits, 2)
+    }
+}
+
+def getReader(input: String) = BitSequenceReader(
+    input.flatMap(hexChar => {
+        val value = Integer.parseInt(hexChar.toString, 16)
+        Seq(8, 4, 2, 1).map(mask => (value & mask) != 0)
+    })
+)
+
+def getPacket(reader: BitSequenceReader): Packet = {
+    val version = reader.readInt(3)
+    val `type` = reader.readInt(3)
+    val packets = ListBuffer.empty[Packet]
+    var payload = 0L
+
+    if (`type` == 4) {
+        var continue = true
+        while (continue) {
+            val isLast = reader.readInt(1) == 0
+            payload = payload * 16 + reader.readInt(4)
+            continue = !isLast
+        }
+    } else if (reader.readInt(1) == 0) {
+        val length = reader.readInt(15)
+        val subPackages = reader.getBitSequenceReader(length)
+        while (subPackages.nonEmpty) {
+            packets += getPacket(subPackages)
+        }
+    } else {
+        val packetCount = reader.readInt(11)
+        packets ++= (for (_ <- 0 until packetCount) yield getPacket(reader))
+    }
+
+    return Packet(version, `type`, payload, packets.toList)
+}
+
+def getTotalVersion(packet: Packet): Int = {
+    return packet.version + packet.packets.map(getTotalVersion).sum
+}
+
+def evaluate(packet: Packet): Long = {
+    val parts = packet.packets.map(evaluate)
+
+    return packet.`type` match {
+        case 0 => parts.sum
+        case 1 => parts.product
+        case 2 => parts.min
+        case 3 => parts.max
+        case 4 => packet.payload
+        case 5 => if parts(0) > parts(1) then 1 else 0
+        case 6 => if parts(0) < parts(1) then 1 else 0
+        case 7 => if parts(0) == parts(1) then 1 else 0
+    }
+}
+
+def evaluatorOne(input: Packet): Int = getTotalVersion(input)
+def evaluatorTwo(input: Packet): Long = evaluate(input)
+
+def readLinesFromFile(filePath: String): Try[List[String]] =
+    Using(Source.fromResource(filePath))(_.getLines().toList)
+
+def hello(): Unit = {
+    readLinesFromFile("day16.txt") match {
+        case Success(lines) => {
+            val input = getPacket(getReader(lines.head))
+            println(s"Part One: ${evaluatorOne(input)}")
+            println(s"Part Two: ${evaluatorTwo(input)}")
+        }
+        case Failure(exception) => {
+            println(s"Error reading file: ${exception.getMessage}")
+        }
+    }
+}
