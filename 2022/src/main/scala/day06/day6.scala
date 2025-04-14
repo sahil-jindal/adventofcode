7a8c0e67987bc83db8b1c9cc3edbe152592208ee
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-04-14 17:17:27 +0000

    Finished 2022 Day 6

---
+++ 2022/src/main/scala/day06/day6.scala
@@ -0,0 +1,26 @@
+package day06
+
+import scala.util.{Try, Success, Failure, Using}
+import scala.io.Source
+
+def startOfBlock(input: String, len: Int): Int = {
+    return len + input.sliding(len).zipWithIndex.collectFirst { case (sub, i) if sub.toSet.size == len => i }.get
+}
+
+def evaluatorOne(input: String): Int = startOfBlock(input, 4)
+def evaluatorTwo(input: String): Int = startOfBlock(input, 14)
+
+def readLinesFromFile(filePath: String): Try[List[String]] =
+    Using(Source.fromResource(filePath))(_.getLines().toList)
+
+def hello(): Unit = {
+    readLinesFromFile("day06.txt") match {
+        case Success(lines) => {
+            println(s"Part One: ${evaluatorOne(lines.head)}")
+            println(s"Part Two: ${evaluatorTwo(lines.head)}")
+        }
+        case Failure(exception) => {
+            println(s"Error reading file: ${exception.getMessage}")
+        }
+    }
+}
