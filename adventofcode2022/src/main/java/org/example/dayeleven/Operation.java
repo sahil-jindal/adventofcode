7d8da8bdcab7df316323937422697956da85a4ab
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-04-16 11:29:28 +0000

    Goodbye Java Solutions

--- adventofcode2022/src/main/java/org/example/dayeleven/Operation.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-abstract class Operation {
-    protected int constantNumber;
-    public Operation(int number) {
-        this.constantNumber = number;
-    }
-    abstract int operate(int old);
-}
