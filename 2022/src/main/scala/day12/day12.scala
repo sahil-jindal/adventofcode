0d5f4a54edc5e9df9b1bfee6fa73b2037fda3f32
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-04-17 18:31:55 +0000

    Finished 2022 Day 12

---
+++ 2022/src/main/scala/day12/day12.scala
@@ -0,0 +1,85 @@
+package day12
+
+import scala.util.{Try, Success, Failure, Using}
+import scala.io.Source
+import scala.collection.mutable
+
+case class Coord(lat: Int, lon: Int)
+case class Symbol(value: Char)
+case class Elevation(value: Char)
+
+// locations on the map will be represented by the following structure of points-of-interests.
+case class Poi(symbol: Symbol, elevation: Elevation, distanceFromGoal: Int)
+
+val startSymbol = Symbol('S');
+val goalSymbol = Symbol('E');
+val lowestElevation = Elevation('a');
+val highestElevation = Elevation('z');
+
+def parseInput(input: List[String]): Map[Coord, Symbol] = {
+    return (for {
+        (line, y) <- input.zipWithIndex
+        (ch, x) <- line.zipWithIndex
+    } yield Coord(x, y) -> Symbol(ch)).toMap
+}
+
+def neighbours(coord: Coord): Seq[Coord] = {
+    return Seq(
+        coord.copy(lat = coord.lat + 1),
+        coord.copy(lat = coord.lat - 1),
+        coord.copy(lon = coord.lon + 1),
+        coord.copy(lon = coord.lon - 1)
+    )
+}
+
+def getElevation(symbol: Symbol): Elevation = symbol.value match {
+    case 'S' => lowestElevation
+    case 'E' => highestElevation
+    case _ => Elevation(symbol.value) 
+}
+
+def getPois(input: List[String]): List[Poi] = {
+    val map = parseInput(input)
+    val goal = map.keys.find(point => map(point) == goalSymbol).get
+
+    val poiByCoord = mutable.Map(goal -> Poi(goalSymbol, getElevation(goalSymbol), 0))
+
+    val pq = mutable.Queue(goal)
+
+    while (pq.nonEmpty) {
+        val thisCoord = pq.dequeue()
+        val thisPoi = poiByCoord(thisCoord)
+        val adjacent = neighbours(thisCoord).filter(map.contains)
+
+        for (nextCoord <- adjacent; if !poiByCoord.contains(nextCoord)) {
+            val nextSymbol = map(nextCoord)
+            val nextElevation = getElevation(nextSymbol)
+
+            if (thisPoi.elevation.value - nextElevation.value <= 1) {
+                poiByCoord(nextCoord) = Poi(nextSymbol, nextElevation, thisPoi.distanceFromGoal + 1)
+                pq.enqueue(nextCoord)
+            }
+        }
+    }
+
+    return poiByCoord.values.toList
+}
+
+def evaluatorOne(pois: List[Poi]): Int = pois.find(_.symbol == startSymbol).get.distanceFromGoal
+def evaluatorTwo(pois: List[Poi]): Int = pois.collect { case Poi(_, elevation, distanceFromGoal) if elevation == lowestElevation => distanceFromGoal }.min
+
+def readLinesFromFile(filePath: String): Try[List[String]] =
+    Using(Source.fromResource(filePath))(_.getLines().toList)
+
+def hello(): Unit = {
+    readLinesFromFile("day12.txt") match {
+        case Success(lines) => {
+            val input = getPois(lines)
+            println(s"Part One: ${evaluatorOne(input)}")
+            println(s"Part Two: ${evaluatorTwo(input)}")
+        }
+        case Failure(exception) => {
+            println(s"Error reading file: ${exception.getMessage}")
+        }
+    }
+}
