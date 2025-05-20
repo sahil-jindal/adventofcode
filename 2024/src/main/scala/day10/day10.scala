6c8cbc4c5a89d3ebdd5f709bf2bffcb1c0a94db6
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-05-20 11:01:30 +0000

    Finished 2024 Day 10

---
+++ 2024/src/main/scala/day10/day10.scala
@@ -0,0 +1,65 @@
+package day10
+
+import scala.util.{Try, Success, Failure, Using}
+import scala.io.Source
+import scala.collection.mutable.{Queue, ListBuffer}
+
+case class Point(y: Int, x: Int)
+
+type Grid = Map[Point, Int]
+
+def parseInput(input: List[String]): Grid = {
+    return (for {
+        (line, y) <- input.zipWithIndex
+        (ch, x) <- line.zipWithIndex
+    } yield Point(y, x) -> ch.asDigit).toMap
+}
+
+def getNeighbours(pos: Point) = Seq(
+    pos.copy(x = pos.x - 1),
+    pos.copy(x = pos.x + 1),
+    pos.copy(y = pos.y - 1),
+    pos.copy(y = pos.y + 1),
+)
+
+def getTrailsFrom(grid: Grid, trailHead: Point): Seq[Point] = {
+    val positions = Queue(trailHead)
+    val trails = ListBuffer.empty[Point]
+
+    while (positions.nonEmpty) {
+        val point = positions.dequeue()
+
+        if (grid(point) == 9) {
+            trails += point
+        } else {
+            for (newPos <- getNeighbours(point)) {
+                if (grid.contains(newPos) && grid(newPos) == grid(point) + 1) {
+                    positions.enqueue(newPos)
+                }
+            }
+        }
+    }
+
+    trails.toSeq
+}
+
+def evaluatorOne(allTrails: Seq[Seq[Point]]): Int = allTrails.map(_.toSet).flatten.size
+def evaluatorTwo(allTrails: Seq[Seq[Point]]): Int = allTrails.flatten.size
+
+def readLinesFromFile(filePath: String): Try[List[String]] =
+    Using(Source.fromResource(filePath))(_.getLines().toList)
+
+def hello(): Unit = {
+    readLinesFromFile("day10.txt") match {
+        case Success(lines) => {
+            val grid = parseInput(lines)
+            val trailHeads = grid.toSeq.collect { case (pos, ch) if ch == 0 => pos }
+            val allTrails = trailHeads.map(pos => getTrailsFrom(grid, pos))
+            println(s"Part One: ${evaluatorOne(allTrails)}")
+            println(s"Part Two: ${evaluatorTwo(allTrails)}")
+        }
+        case Failure(exception) => {
+            println(s"Error reading file: ${exception.getMessage}")
+        }
+    }
+}
