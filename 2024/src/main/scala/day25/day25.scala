0b24d20aa81669dd2d578e086faa5153994e172d
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-05-29 02:47:58 +0000

    Finished 2024 Day 25

---
+++ 2024/src/main/scala/day25/day25.scala
@@ -0,0 +1,34 @@
+package day25
+
+import scala.util.{Try, Success, Failure, Using}
+import scala.io.Source
+
+def groupLines(input: List[String]): List[List[String]] = {
+    return input.foldLeft(List(List.empty[String])) {
+        case (acc, "") => acc :+ List.empty[String]
+        case (acc, elem) => acc.init :+ (acc.last :+ elem)
+    }.filter(_.nonEmpty)
+}
+
+def parsePattern(input: List[String]) = input.transpose.map(_.count(_ == '#'))
+
+def checkMatch(k: List[Int], l: List[Int]) = (k zip l).forall { case (a, b) => a + b <= 7 }
+
+def solver(input: List[String]): Int = {
+    val patterns = groupLines(input)
+
+    val keys = patterns.filter(p => p(0)(0) == '.').map(parsePattern)
+    val locks = patterns.filter(p => p(0)(0) == '#').map(parsePattern)
+
+    return keys.map(k => locks.count(l => checkMatch(k, l))).sum
+}
+
+def readLinesFromFile(filePath: String): Try[List[String]] =
+    Using(Source.fromResource(filePath))(_.getLines().toList)
+
+def hello(): Unit = {
+    readLinesFromFile("day25.txt") match {
+        case Success(lines) => println(s"Answer: ${solver(lines)}")
+        case Failure(exception) => println(s"Error reading file: ${exception.getMessage}")
+    }
+}
