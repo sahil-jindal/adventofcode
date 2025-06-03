de0809d3b3bdb6d421260d39a1d9b236008aab3c
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-06-03 17:01:03 +0000

    Oops! Removed @main from 2019 day 22

--- 2019/src/main/scala/day18/day18.scala
+++ 2019/src/main/scala/day18/day18.scala
@@ -120,7 +120,6 @@ def evaluatorTwo(input: List[String]): Int = {
 def readLinesFromFile(filePath: String): Try[List[String]] =
     Using(Source.fromResource(filePath))(_.getLines().toList)
 
-@main
 def hello(): Unit = {
     readLinesFromFile("day18.txt") match {
         case Success(lines) => {
