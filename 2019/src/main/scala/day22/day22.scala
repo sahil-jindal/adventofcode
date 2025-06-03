560d282d66f342c24cb65a717d5319242ff08b26
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-06-03 17:01:58 +0000

    Refactored 2019 Day 22

--- 2019/src/main/scala/day22/day22.scala
+++ 2019/src/main/scala/day22/day22.scala
@@ -2,52 +2,48 @@ package day22
 
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
-import java.math.BigInteger
 
-def mod(a: BigInteger, m: BigInteger): BigInteger = (a.mod(m).add(m)).mod(m)
-def modInv(a: BigInteger, m: BigInteger): BigInteger = a.modPow(m.subtract(BigInteger.TWO), m)
-
-def parse(input: List[String], m: BigInteger, n: BigInteger): (BigInteger, BigInteger) = {
-    var a = BigInteger.ONE
-    var b = BigInteger.ZERO
+def parse(input: List[String], m: BigInt, n: BigInt): (BigInt, BigInt) = {
+    var a = BigInt(1)
+    var b = BigInt(0)
 
     for (line <- input) {
         line match {
             case l if l.contains("into new stack") => {
-                a = a.negate()
-                b = m.subtract(b).subtract(BigInteger.ONE)
+                a = -a
+                b = m - b - BigInt(1)
             }
             case l if l.startsWith("cut") => {
-                val i = new BigInteger(line.split(" ").last)
-                b = mod(m.add(b).subtract(i), m)
+                val i = BigInt(line.split(" ").last)
+                b = m + b - i
             }
             case l if l.contains("increment") => {
-                val i = new BigInteger(line.split(" ").last)
-                a = mod(a.multiply(i), m)
-                b = mod(b.multiply(i), m)
+                val i = BigInt(line.split(" ").last)
+                a *= i
+                b *= i
             }
             case _ => throw new Exception("Invalid instruction")
         }
     }
 
     val resA = a.modPow(n, m)
-    val resB = mod(b.multiply(resA.subtract(BigInteger.ONE)).multiply(modInv(a.subtract(BigInteger.ONE), m)), m)
+    val resB = (b * (resA - BigInt(1)) * (a - BigInt(1)).modInverse(m)).mod(m)
 
     return (resA, resB)
 }
 
-def evaluatorOne(input: List[String]): BigInteger = {
-    val m = BigInteger.valueOf(10007)
-    val iter = BigInteger.ONE
+def evaluatorOne(input: List[String]): BigInt = {
+    val m = BigInt(10007)
+    val iter = BigInt(1)
     val (a, b) = parse(input, m, iter)
-    return mod(a.multiply(BigInteger.valueOf(2019)).add(b), m)
+    return (a * BigInt(2019) + b).mod(m)
 }
 
-def evaluatorTwo(input: List[String]): BigInteger = {
-    val m = BigInteger.valueOf(119315717514047L)
-    val iter = BigInteger.valueOf(101741582076661L)
+def evaluatorTwo(input: List[String]): BigInt = {
+    val m = BigInt(119315717514047L)
+    val iter = BigInt(101741582076661L)
     val (a, b) = parse(input, m, iter)
-    return mod(modInv(a, m).multiply(BigInteger.valueOf(2020).subtract(b)), m)
+    return (a.modInverse(m) * (BigInt(2020) - b)).mod(m)
 }
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
