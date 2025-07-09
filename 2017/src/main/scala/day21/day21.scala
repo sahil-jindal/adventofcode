82e6bc4a4492660777162f2c47bfe06b94c55cbc
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-07-09 14:54:55 +0000

    Refactored 2017 Day 21

--- 2017/src/main/scala/day21/day21.scala
+++ 2017/src/main/scala/day21/day21.scala
@@ -2,20 +2,19 @@ package day21
 
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
-import scala.collection.mutable.{Map, ListBuffer}
+import scala.collection.mutable.{Map => MutableMap}
 
 class Mtx(val size: Int) {
+    require(size % 2 == 0 || size % 3 == 0)
+
     private val flags = Array.ofDim[Boolean](size, size)
 
-    def codeNumber: Int = {
-        if (size != 2 && size != 3) throw new IllegalArgumentException()
-        return flags.flatten.zipWithIndex.collect { case (true, i) => 1 << i }.sum
-    }
+    def codeNumber = flags.flatten.zipWithIndex.collect { case (true, i) => 1 << i }.sum
 
     def apply(y: Int, x: Int): Boolean = flags(y)(x)
     def update(y: Int, x: Int, value: Boolean): Unit = flags(y)(x) = value
 
-    def flip(): Mtx = {
+    private def flip(): Mtx = {
         val res = new Mtx(size)
         
         for (y <- 0 until size; x <- 0 until size) {
@@ -25,7 +24,7 @@ class Mtx(val size: Int) {
         return res
     }
 
-    def rotate(): Mtx = {
+    private def rotate(): Mtx = {
         val res = new Mtx(size)
         
         for (i <- 0 until size; j <- 0 until size) {
@@ -35,12 +34,16 @@ class Mtx(val size: Int) {
         return res
     }
 
+    def variations(): Seq[Mtx] = {
+        val variants = Iterator.iterate(this, 4)(_.rotate()).toSeq
+        return variants ++ variants.map(_.flip())
+    }
+
     def split(): Seq[Mtx] = {
-        
+
         val blockSize = size match {
             case s if s % 2 == 0 => 2
             case s if s % 3 == 0 => 3
-            case _ => throw new Exception()
         }
         
         return (for {
@@ -89,54 +92,45 @@ object Mtx {
     }
 }
 
-class RuleSet(input: List[String]) {
-    private val rules2 = Map.empty[Int, Mtx]
-    private val rules3 = Map.empty[Int, Mtx]
-
-    for (line <- input) {
-        val Array(left, right) = line.split(" => ")
-        
-        val rules = left.length match {
-            case 5  => rules2
-            case 11 => rules3
-            case _  => throw new Exception()
-        }
-        
-        for (mtx <- variations(Mtx.fromString(left))) {
-            rules(mtx.codeNumber) = Mtx.fromString(right)
-        }
-    }
-    
-
+class RuleSet(rules2: Map[Int, Mtx], rules3: Map[Int, Mtx]) {
     def apply(mtx: Mtx): Mtx = Mtx.join(mtx.split().map {
         case child if child.size == 2 => rules2(child.codeNumber)
         case child if child.size == 3 => rules3(child.codeNumber)
     })
-
-    def variations(mtx: Mtx): Seq[Mtx] = {
-        var variants = ListBuffer(mtx)
-        
-        for (_ <- 0 until 4) {
-            variants += variants.last.rotate()
-        }
-        
-        return (variants ++ variants.map(_.flip())).toSeq
-    }
 }
 
-def iterate(input: List[String], iterations: Int): Int = {
+def parseInput(input: List[String]): RuleSet = {
+    val rules2 = MutableMap.empty[Int, Mtx]
+    val rules3 = MutableMap.empty[Int, Mtx]
+
+    for (line <- input) {
+        val Array(leftMtx, rightMtx) = line.split(" => ").map(Mtx.fromString)
+        
+        val rules = leftMtx.size match {
+            case 2 => rules2
+            case 3 => rules3
+        }
+        
+        for (mtx <- leftMtx.variations()) {
+            rules(mtx.codeNumber) = rightMtx
+        }
+    }
+
+    return RuleSet(rules2.toMap, rules3.toMap)
+}
+
+def iterate(ruleSet: RuleSet, iterations: Int): Int = {
     var mtx = Mtx.fromString(".#./..#/###")
-    val ruleset = RuleSet(input)
     
     for (_ <- 0 until iterations) {
-        mtx = ruleset.apply(mtx)
+        mtx = ruleSet.apply(mtx)
     }
     
     return mtx.count()
 }
     
-def evaluatorOne(input: List[String]): Int = iterate(input, 5)
-def evaluatorTwo(input: List[String]): Int = iterate(input, 18)
+def evaluatorOne(ruleSet: RuleSet): Int = iterate(ruleSet, 5)
+def evaluatorTwo(ruleSet: RuleSet): Int = iterate(ruleSet, 18)
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
     Using(Source.fromResource(filePath))(_.getLines().toList)
@@ -144,8 +138,9 @@ def readLinesFromFile(filePath: String): Try[List[String]] =
 def hello(): Unit = {
     readLinesFromFile("day21.txt") match {
         case Success(lines) => {
-            println(s"Part One: ${evaluatorOne(lines)}")
-            println(s"Part Two: ${evaluatorTwo(lines)}")
+            val input = parseInput(lines)
+            println(s"Part One: ${evaluatorOne(input)}")
+            println(s"Part Two: ${evaluatorTwo(input)}")
         }
         case Failure(exception) => {
             println(s"Error reading file: ${exception.getMessage}")
