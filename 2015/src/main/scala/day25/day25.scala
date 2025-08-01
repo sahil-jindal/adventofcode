d9b1f71e976c7eb738c43f7222488a01acf1a6a7
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-08-01 11:41:25 +0000

    Changed Loop to BigInt

--- 2015/src/main/scala/day25/day25.scala
+++ 2015/src/main/scala/day25/day25.scala
@@ -3,19 +3,15 @@ package day25
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
 
-def solver(input: String): Long = {
+def solver(input: String): Int = {
     val Seq(rowDst, colDst) = raw"(\d+)".r.findAllIn(input).map(_.toInt).toSeq
 
     val d = rowDst + colDst - 1
     val k = (d * (d - 1)) / 2 + colDst // Anti-diagonal encoding
 
-    var m = 20151125L
-
-    for (_ <- 1 until k) {
-        m = (m * 252533L) % 33554393L
-    }
+    val m = BigInt(33554393)
     
-    return m
+    return (BigInt(20151125) * BigInt(252533).modPow(k - 1, m)).mod(m).intValue()
 }
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
