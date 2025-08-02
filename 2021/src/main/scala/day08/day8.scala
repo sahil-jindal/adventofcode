7db5efc1974f294c20427de473673900db01e9da
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-08-02 07:58:36 +0000

    Refactor AdventofCode 2021 Day 8

--- 2021/src/main/scala/day08/day8.scala
+++ 2021/src/main/scala/day08/day8.scala
@@ -10,47 +10,43 @@ def parseInput(input: List[String]) = input.map(line => {
     Pair(first, second)
 })
 
-def evaluatorOne(input: List[Pair]): Int = {
-    // we can identify digits 1, 7, 4 and 8 by their active segments count:
-    val segmentCounts = Seq("cf", "acf", "bcdf", "abcdefg").map(_.length).toSet
-    return input.flatMap(_.second).count(it => segmentCounts.contains(it.length))
-}
+def decodeOutput(pair: Pair): Int = {
+    val patterns = pair.first.map(_.toSet)
 
-def evaluatorTwo(input: List[Pair]): Int = {
-    var res = 0
+    val digits = Array.ofDim[Set[Char]](10)
 
-    for (pair <- input) {
-        val patterns = pair.first.map(_.toSet)
+    digits(1) = patterns.find(_.size == "cf".length).get
+    digits(4) = patterns.find(_.size == "bcdf".length).get
 
-        val digits = Array.ofDim[Set[Char]](10)
-
-        digits(1) = patterns.find(_.size == "cf".length).get
-        digits(4) = patterns.find(_.size == "bcdf".length).get
-
-        def lookup(segmentCount: Int, commonWithOne: Int, commonWithFour: Int) = {
-            patterns.find(pattern => 
-                pattern.size == segmentCount && 
-                (pattern & digits(1)).size == commonWithOne && 
-                (pattern & digits(4)).size == commonWithFour
-            ).get
-        }
-
-        digits(0) = lookup(6, 2, 3)  
-        digits(2) = lookup(5, 1, 2)
-        digits(3) = lookup(5, 2, 3)
-        digits(5) = lookup(5, 1, 3)
-        digits(6) = lookup(6, 1, 3)
-        digits(7) = lookup(3, 2, 2)
-        digits(8) = lookup(7, 2, 4)
-        digits(9) = lookup(6, 2, 4)
-
-        def decode(v: String) = digits.indexOf(v.toSet)
-
-        res += pair.second.map(decode).mkString.toInt
+    def lookup(segmentCount: Int, commonWithOne: Int, commonWithFour: Int) = {
+        patterns.find(pattern => 
+            pattern.size == segmentCount && 
+            (pattern & digits(1)).size == commonWithOne && 
+            (pattern & digits(4)).size == commonWithFour
+        ).get
     }
 
-    return res
+    digits(0) = lookup(6, 2, 3)  
+    digits(2) = lookup(5, 1, 2)
+    digits(3) = lookup(5, 2, 3)
+    digits(5) = lookup(5, 1, 3)
+    digits(6) = lookup(6, 1, 3)
+    digits(7) = lookup(3, 2, 2)
+    digits(8) = lookup(7, 2, 4)
+    digits(9) = lookup(6, 2, 4)
+
+    def decode(v: String) = digits.indexOf(v.toSet)
+
+    return pair.second.map(decode).mkString.toInt
 }
+
+def evaluatorOne(input: List[Pair]): Int = {
+    // we can identify digits 1, 7, 4 and 8 by their active segments count:
+    val segmentCounts = Set("cf", "acf", "bcdf", "abcdefg").map(_.length)
+    return input.flatMap(_.second).map(_.length).count(segmentCounts.contains)
+}
+
+def evaluatorTwo(input: List[Pair]): Int = input.map(decodeOutput).sum
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
     Using(Source.fromResource(filePath))(_.getLines().toList)
