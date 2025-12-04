83114ae46ad8e014b4fd8f8d92e5bb066877785e
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-12-04 05:33:44 +0000

    Completed AOC 2025 Day 2

---
+++ 2025/src/main/scala/day02/day2.scala
@@ -0,0 +1,36 @@
+package day02
+
+import scala.collection.immutable.NumericRange.Inclusive
+import scala.util.{Try, Success, Failure, Using}
+import scala.io.Source
+import scala.util.matching.Regex
+
+val partOne = raw"(\d+)\1".r
+val partTwo = raw"(\d+?)\1+".r
+
+def parseInput(input: String) = input.split(',').map(line => {
+    val Array(start, end) = line.split('-').map(_.toLong)
+    start to end
+})
+
+def helper(ranges: Array[Inclusive[Long]], pattern: Regex) = 
+    ranges.flatMap(_.filter(it => pattern.matches(it.toString))).sum
+
+def evaluatorOne(ranges: Array[Inclusive[Long]]) = helper(ranges, partOne)
+def evaluatorTwo(ranges: Array[Inclusive[Long]]) = helper(ranges, partTwo)
+
+def readLinesFromFile(filePath: String): Try[List[String]] =
+    Using(Source.fromResource(filePath))(_.getLines().toList)
+
+def hello(): Unit = {
+    readLinesFromFile("day02.txt") match {
+        case Success(lines) => {
+            val input = parseInput(lines.head)
+            println(s"Part One: ${evaluatorOne(input)}")
+            println(s"Part Two: ${evaluatorTwo(input)}")
+        }
+        case Failure(exception) => {
+            println(s"Error reading file: ${exception.getMessage}")
+        }
+    }
+}
