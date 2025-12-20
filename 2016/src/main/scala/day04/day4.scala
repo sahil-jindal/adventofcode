100fd7554a2a69a2563228f355ade110b21f6a56
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-12-20 15:28:44 +0000

    Refactoring 1/n

--- 2016/src/main/scala/day04/day4.scala
+++ 2016/src/main/scala/day04/day4.scala
@@ -3,7 +3,9 @@ package day04
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
 
-case class Room(encrypted: String, id: Int, checkSum: String)
+case class Room(encrypted: String, id: Int, checkSum: String) {
+    def decryptedRoom = encrypted.map(ch => ('a' + (ch - 'a' + id) % 26).toChar).mkString
+}
 
 def parseInput(input: List[String]) = input.map(line => { 
     val List(first, second, third) = raw"([^\d]+)\-(\d+)\[(.*)\]".r.findFirstMatchIn(line).get.subgroups
@@ -20,10 +22,8 @@ def getRealRooms(rooms: List[Room]) = rooms.filter(room => {
     computedhash == room.checkSum
 })
 
-def decryptedRoom(room: Room): String = room.encrypted.map(ch => ('a' + (ch - 'a' + room.id) % 26).toChar).mkString
-
 def evaluatorOne(rooms: List[Room]): Int = rooms.map(_.id).sum
-def evaluatorTwo(rooms: List[Room]): Int = rooms.find(decryptedRoom(_).contains("northpole")).get.id
+def evaluatorTwo(rooms: List[Room]): Int = rooms.find(_.decryptedRoom.contains("northpole")).get.id
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
     Using(Source.fromResource(filePath))(_.getLines().toList)
