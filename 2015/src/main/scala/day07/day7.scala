7aa9229dd6f06c96262d3beca2775b2cc7003fc7
sahil-jindal <sahiljindal.thebest@gmail.com>
2026-03-19 14:32:40 +0000

    Improved sealed class

--- 2015/src/main/scala/day07/day7.scala
+++ 2015/src/main/scala/day07/day7.scala
@@ -5,8 +5,10 @@ import scala.io.Source
 import scala.collection.mutable.{Map => MutableMap}
 
 sealed trait Gate
+case class Bit(a: String) extends Gate
 case class Not(a: String) extends Gate
 case class Wire(a: String) extends Gate
+case class Constant(a: Int) extends Gate
 case class Or(a: String, b: String) extends Gate
 case class And(a: String, b: String) extends Gate
 case class LeftShift(a: String, n: Int) extends Gate
@@ -14,10 +16,12 @@ case class RightShift(a: String, n: Int) extends Gate
 
 def getGate(str: String) = str match {
     case s"NOT $x" => Not(x)
+    case s"1 AND $x" => Bit(x)
     case s"$x OR $y" => Or(x, y)
     case s"$x AND $y" => And(x, y)
     case s"$x LSHIFT $y" => LeftShift(x, y.toInt)
     case s"$x RSHIFT $y" => RightShift(x, y.toInt)
+    case other if other.matches("-?\\d+")=> Constant(other.toInt)
     case other => Wire(other)
 }
 
@@ -27,11 +31,11 @@ def parseInput(input: List[String]) = input.map(line => {
 }).toMap
 
 def evaluate(wire: String, instructions: Map[String, Gate], cache: MutableMap[String, Int]): Int = {
-    if (wire.forall(_.isDigit)) return wire.toInt
-
     return cache.getOrElseUpdate(wire, { 
         instructions(wire) match {
-            case Wire(s) => evaluate(s, instructions, cache) // Direct assignment
+            case Constant(a) => a
+            case Wire(s) => evaluate(s, instructions, cache)
+            case Bit(a) => evaluate(a, instructions, cache) & 1
             case Not(a) => ~evaluate(a, instructions, cache) & 0xFFFF
             case Or(a, b) => evaluate(a, instructions, cache) | evaluate(b, instructions, cache)
             case And(a, b) => evaluate(a, instructions, cache) & evaluate(b, instructions, cache)
@@ -45,7 +49,7 @@ def solver(input: List[String]): (Int, Int) = {
     val originalInstructions = parseInput(input)
     val signalA = evaluate("a", originalInstructions, MutableMap.empty)
     
-    val modifiedInstructions = originalInstructions + ("b" -> Wire(signalA.toString))
+    val modifiedInstructions = originalInstructions + ("b" -> Constant(signalA))
     val newSignalA = evaluate("a", modifiedInstructions, MutableMap.empty)
     
     return (signalA, newSignalA)
