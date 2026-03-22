9ee92848fef0051b8536103ad22495e2ab710e60
sahil-jindal <sahiljindal.thebest@gmail.com>
2026-03-22 17:16:45 +0000

    Rewrote AOC 2015 Day 17

--- 2015/src/main/scala/day17/day17.scala
+++ 2015/src/main/scala/day17/day17.scala
@@ -3,27 +3,40 @@ package day17
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
 
-val totalLiters = 150
+extension (self: Map[Int, Int]) {
+    def incrementKeys(): Map[Int, Int] = {
+        return self.map { case (k, v) => (k + 1) -> v }
+    }
+
+    def |+|(that: Map[Int, Int]): Map[Int, Int] = {
+        val common = self.keySet & that.keySet
+        val delta = (self ++ that) -- common
+
+        val combination = common.map(it => {
+            it -> (self(it) + that(it))
+        }).toMap
+
+        return delta ++ combination
+    }
+}
+
+val goal = 150
 
 def parseInput(input: List[String]) = input.map(_.toInt).sorted
 
-def findAllCombinations(containers: List[Int], target: Int): List[List[Int]] = {
-    if (target < 0) return Nil
-    if (target == 0) return List(Nil)
-    if (containers.isEmpty) return Nil
-    
-    val (curr, rest) = (containers.head, containers.tail)
+def preComputation(input: List[Int]): Map[Int, Int] = {
+    val ways = Array.fill(goal + 1)(Map.empty[Int, Int])
+    ways(0) = Map(0 -> 1)
 
-    findAllCombinations(rest, target - curr).map(curr :: _) :::
-    findAllCombinations(rest, target)
+    for (item <- input; i <- goal to item by -1) {
+        ways(i) = ways(i) |+| ways(i - item).incrementKeys()
+    }
+
+    return ways(goal)
 }
 
-def solver(lines: List[String]): (Int, Int) = {
-    val containers = parseInput(lines)
-    val combinations = findAllCombinations(containers, totalLiters)
-    val minContainerCount = combinations.map(_.size).min
-    return (combinations.size, combinations.count(_.size == minContainerCount))
-}
+def evaluatorOne(input: Map[Int, Int]) = input.values.sum
+def evaluatorTwo(input: Map[Int, Int]) = input.minBy(_._1)._2
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
     Using(Source.fromResource(filePath))(_.getLines().toList)
@@ -31,9 +44,9 @@ def readLinesFromFile(filePath: String): Try[List[String]] =
 def hello(): Unit = {
     readLinesFromFile("day17.txt") match {
         case Success(lines) => {
-            val (partOne, partTwo) = solver(lines)
-            println(s"Part One: $partOne")
-            println(s"Part Two: $partTwo")
+            val input = preComputation(parseInput(lines))
+            println(s"Part One: ${evaluatorOne(input)}")
+            println(s"Part Two: ${evaluatorTwo(input)}")
         }
         case Failure(exception) => {
             println(s"Error reading file: ${exception.getMessage}")
