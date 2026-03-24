7ce7eb46b60b0ff1ab313e768689aec8f1afeae7
sahil-jindal <sahiljindal.thebest@gmail.com>
2026-03-24 21:27:51 +0000

    Rewrote AOC 2017 Day 7

--- 2017/src/main/scala/day07/day7.scala
+++ 2017/src/main/scala/day07/day7.scala
@@ -2,50 +2,114 @@ package day07
 
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
+import scala.collection.mutable.Queue
+import scala.util.control.Breaks._
 
-case class Node(id: String, children: List[String], weight: Int, var treeWeight: Int = -1)
+extension (arr: Array[Int]) {
+    def swap(a: Int, b: Int): Unit = {
+        val temp = arr(a)
+        arr(a) = arr(b)
+        arr(b) = temp
+    }
+}
+
+case class TreeNode(weight: Int, children: List[String])
+
+case class Node(
+    var hasParent: Boolean = false,
+    var parent: String = "",
+    var children: Int = 0,
+    var processed: Int = 0,
+    var weight: Int = 0,
+    var total: Int = 0,
+    val subWeights: Array[Int] = Array(0, 0),
+    val subTotals: Array[Int] = Array(0, 0),
+)
 
 val pattern =  raw"(\w+) \((\d+)\)(?: -> ([\w, ]+))?".r
 
-type Tree = Map[String, Node]
-
-def parseInput(input: List[String]): Tree = {    
-    return input.map(line => {
-        val List(id, weight, childrenOpt) = pattern.findFirstMatchIn(line).get.subgroups
+def parseInput(input: List[String]) = input.collect {
+    case pattern(id, weight, childrenOpt) => {
         val children = Option(childrenOpt).map(_.split(", ").toList).getOrElse(List.empty)
-        id -> Node(id, children, weight.toInt)
-    }).toMap
-}
+        id -> TreeNode(weight.toInt, children)
+    }
+}.toMap
 
-def root(tree: Tree): Node = {
-    val allNodes = tree.keySet
-    val allChildren = tree.values.flatMap(_.children).toSet
-    return tree(allNodes.diff(allChildren).head)
-}
+def solver(pairs: Map[String, TreeNode]): (String, Int) = {
+    val nodes = pairs.keySet.map(_ -> Node()).toMap
+
+    for ((id, TreeNode(weight, children)) <- pairs) {
+        nodes(id).weight = weight
+        nodes(id).total = weight
+        nodes(id).children = children.size
+
+        for (edge <- children) {
+            nodes(edge).parent = id
+            nodes(edge).hasParent = true
+        }
+    }
+
+    // The root is the only node without a parent. Start from any node, 
+    // and walk up the tree until finding the root.
+    var partOne = pairs.keySet.head
+
+    while (nodes(partOne).hasParent) {
+        partOne = nodes(partOne).parent
+    }
+
+    val todo = Queue.from(nodes.collect { 
+        case (id, node) if node.children == 0 => id 
+    })
     
-def computeTreeWeights(node: Node, tree: Tree): Int = {
-    node.treeWeight = node.weight + node.children.map(childId => computeTreeWeights(tree(childId), tree)).sum
-    return node.treeWeight
-}
+    var partTwo = 0
 
-def bogusChild(node: Node, tree: Tree): Node = {
-    val grouped = node.children.map(tree).groupBy(_.treeWeight).values.toSeq.sortBy(_.size)
-    return if grouped.size == 1 then null else grouped.head.head
-}
-    
-def fix(node: Node, desiredWeight: Int, tree: Tree): Int = {
-    val bogus = bogusChild(node, tree)
-    if bogus == null then return desiredWeight - node.treeWeight + node.weight
-    return fix(bogus, desiredWeight - node.treeWeight + bogus.treeWeight, tree)
-}
+    breakable {
+        while (todo.nonEmpty) {
+            val index = todo.dequeue()
+            val Node(_, parent, _, _, weight, total, _, _) = nodes(index)
+            val node = nodes(parent)
 
-def solver(tree: Tree): (String, Int) = {
-    val rootNode = root(tree)
-    computeTreeWeights(rootNode, tree)
-    val bogusChildNode = bogusChild(rootNode, tree)
-    val desiredWeight = tree(rootNode.children.find(_ != bogusChildNode.id).get).treeWeight
-    val correctWeight = fix(bogusChildNode, desiredWeight, tree)
-    return (rootNode.id, correctWeight)
+            if (node.processed < 2) {
+                 // Fill out the first two children in any order.
+                node.subWeights(node.processed) = weight
+                node.subTotals(node.processed) = total
+            } else {
+                // Representing the balanced nodes as `b` and the unbalanced node as `u`,
+                // there are 4 possibilities:
+                // b3 + [b1 b2] => [b2 b1] Swap, keep accumulating
+                // b3 + [b1 u2] => [u2 b1] Swap, unbalanced node identified
+                // u3 + [b1 b2] -> [u3 b2] Overwrite, unbalanced node identified
+                // b3 + [u1 b2] => [u1 b2] Do nothing, unbalanced node identified
+                // The unbalanced node will always be first (if it exists).
+                if (node.subTotals(0) == total) {
+                    node.subWeights.swap(0, 1)
+                    node.subTotals.swap(0, 1)
+                } else if (node.subTotals(1) != total) {
+                    node.subWeights(0) = weight
+                    node.subTotals(0) = total
+                }
+            
+                // If the unbalanced node was identified, it is now first, 
+                // and we can short-circuit summing the weights of the rest of the tree.
+                val Array(x, y) = node.subTotals
+
+                if (x != y) {
+                    partTwo = node.subWeights(0) - x + y
+                    break()
+                }
+            }
+
+            node.total += total
+            node.processed += 1
+
+            // If we've processed all children then add to the queue and check balance.
+            if (node.processed == node.children) {
+                todo.enqueue(parent)
+            }
+        }
+    }
+
+    return (partOne, partTwo)
 }
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
