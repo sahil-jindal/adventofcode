c2df3353117f357fb42f775e972dbb27c7ddd13e
sahil-jindal <sahiljindal.thebest@gmail.com>
2026-03-24 21:38:34 +0000

    Rewrote AOC 2017 Day 13

--- 2017/src/main/scala/day13/day13.scala
+++ 2017/src/main/scala/day13/day13.scala
@@ -3,18 +3,49 @@ package day13
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
 
-case class Layer(depth: Int, range: Int)
+// NOTE: This intentionally violates minimal representation.
+// period, prevlcm, and currlcm are mathematically related,
+// but we store them explicitly to enable stateless, purely mechanical folding.
+case class State(depth: Int, period: Int, prevlcm: Int, currlcm: Int)
+
+case class Layer(depth: Int, range: Int) {
+    val period = 2 * (range - 1)
+}
 
 def parseInput(input: List[String]) = input.collect {
     case s"$depth: $range" => Layer(depth.toInt, range.toInt)
 }
 
-def severities(layers: List[Layer], t: Int): List[Int] = {
-    return layers.withFilter(it => (t + it.depth) % (2 * it.range - 2) == 0).map(it => it.depth * it.range)
+def gcd(a: Int, b: Int): Int = if b == 0 then a else gcd(b, a % b)
+def lcm(a: Int, b: Int): Int = a * b / gcd(a, b)
+
+def helper(current: IndexedSeq[Int], state: State): IndexedSeq[Int] = {
+    val State(depth, period, prevlcm, currlcm) = state
+
+    return (for {
+        extra <- 0 until currlcm by prevlcm
+        delay <- current
+        if (delay + extra + depth) % period != 0
+    } yield delay + extra)
 }
 
-def evaluatorOne(layers: List[Layer]): Int = severities(layers, 0).sum
-def evaluatorTwo(layers: List[Layer]): Int = Iterator.from(0).find(severities(layers, _).isEmpty).get
+def evaluatorOne(layers: List[Layer]): Int = {
+    return layers.withFilter(it => it.depth % it.period == 0).map(it => it.depth * it.range).sum
+}
+
+def evaluatorTwo(layers: List[Layer]): Int = {
+    val newLayers = layers.sortBy(_.range)
+    val depths = newLayers.map(_.depth)
+    val periods = newLayers.map(_.period)
+
+    val lcms = periods.scanLeft(1)(lcm)
+
+    val states = List.tabulate(layers.size) { i =>
+        State(depths(i), periods(i), lcms(i), lcms(i + 1))
+    }
+
+    return states.foldLeft(IndexedSeq(1))(helper).head
+}
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
     Using(Source.fromResource(filePath))(_.getLines().toList)
