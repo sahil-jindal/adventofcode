e7cb32ccb023d94a1942f33f3f038a68ab1bdf5a
sahil-jindal <sahiljindal.thebest@gmail.com>
2026-03-28 04:35:58 +0000

    Rewrote AOC 2015 Day 24

--- 2015/src/main/scala/day24/day24.scala
+++ 2015/src/main/scala/day24/day24.scala
@@ -3,23 +3,55 @@ package day24
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
 
-def parseInput(input: List[String]) = input.map(_.toLong).sorted
-
-def findCombinations(numbers: List[Long], len: Int, target: Long) = {
-    numbers.combinations(len).filter(_.sum == target)
+extension (a: Int) {
+    def saturatingAdd(b: Int): Int = {
+        val sum = a.toLong + b.toLong
+        if (sum > Int.MaxValue) return Int.MaxValue
+        if (sum < Int.MinValue) return Int.MinValue
+        return sum.toInt
+    }
 }
 
-def solver(numbers: List[Long], groupLength: Int): Long = {
-    val target = numbers.sum / groupLength
-
-    return (1 to numbers.length).iterator
-        .map(findCombinations(numbers, _, target))
-        .find(_.nonEmpty).get
-        .map(_.product).min
+extension (a: Long) {
+    def saturatingMul(b: Long): Long = {
+        val r = BigInt(a) * BigInt(b)
+        if (r > Long.MaxValue) return Long.MaxValue
+        if (r < Long.MinValue) return Long.MinValue
+        return r.toLong
+    }
 }
 
-def evaluatorOne(numbers: List[Long]): Long = solver(numbers, 3)
-def evaluatorTwo(numbers: List[Long]): Long = solver(numbers, 4)
+def parseInput(input: List[String]) = input.map(_.toInt).sorted
+
+def solver(numbers: List[Int], groupLength: Int): Long = {
+    val goal = numbers.sum / groupLength
+
+    val minimum = Array.fill(goal + 1)(Int.MaxValue)
+    minimum(0) = 0
+
+    val qe = Array.fill(goal + 1)(Long.MaxValue)
+    qe(0) = 1
+
+    for (item <- numbers; i <- goal to item by -1) {
+        val take = minimum(i - item).saturatingAdd(1)
+        val notTake = minimum(i)
+
+        if (take < notTake) {
+            // Taking the item result in fewer packages, use the new quantum entanglement 
+            // even if it's greater than the existing value.
+            minimum(i) = take
+            qe(i) = qe(i - item).saturatingMul(item)
+        } else if (take == notTake) {
+            // Number of packages is the same, so choose the minimum quantum entanglement.
+            qe(i) = qe(i).min(qe(i - item).saturatingMul(item))
+        }
+    }
+
+    return qe(goal)
+}
+
+def evaluatorOne(numbers: List[Int]): Long = solver(numbers, 3)
+def evaluatorTwo(numbers: List[Int]): Long = solver(numbers, 4)
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
     Using(Source.fromResource(filePath))(_.getLines().toList)
