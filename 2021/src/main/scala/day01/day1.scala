6851b82f0230d2e5390671f7cca66113170aef13
sahil-jindal <sahiljindal.thebest@gmail.com>
2026-03-28 00:06:00 +0000

    Rewrote AOC 2021 Day 1

--- 2021/src/main/scala/day01/day1.scala
+++ 2021/src/main/scala/day01/day1.scala
@@ -5,12 +5,8 @@ import scala.io.Source
 
 def parseInput(input: List[String]) = input.map(_.toInt)
 
-def depthIncrease(ns: List[Int]): Int = (ns.init zip ns.tail).count { case (a, b) => a < b }
-
-def threeMeasurements(ns: List[Int]) = ns.sliding(3).map(_.sum).toList
-
-def evaluatorOne(ns: List[Int]): Int = depthIncrease(ns)
-def evaluatorTwo(ns: List[Int]): Int = depthIncrease(threeMeasurements(ns))
+def evaluatorOne(ns: List[Int]): Int = ns.sliding(2).count(w => w(0) < w(1))
+def evaluatorTwo(ns: List[Int]): Int = ns.sliding(4).count(w => w(0) < w(3))
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
     Using(Source.fromResource(filePath))(_.getLines().toList)
