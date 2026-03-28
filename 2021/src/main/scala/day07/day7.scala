bc9022dfb5fc2f095985e9e07100bf785cb45af3
sahil-jindal <sahiljindal.thebest@gmail.com>
2026-03-28 00:47:23 +0000

    Rewrote AOC 2021 Day 7

--- 2021/src/main/scala/day07/day7.scala
+++ 2021/src/main/scala/day07/day7.scala
@@ -3,16 +3,37 @@ package day07
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
 
-def parseInput(input: String) = input.split(",").map(_.toInt).toList
-
-def fuelMin(positions: List[Int], fuelConsumption: Int => Int): Int = {
-    return (positions.min to positions.max).map(target => {
-        positions.map(it => fuelConsumption((target - it).abs)).sum
-    }).min
+extension (a: Int) {
+    def midpoint(b: Int): Int = {
+        return (a & b) + ((a ^ b) >> 1)
+    }
 }
 
-def evaluatorOne(positions: List[Int]): Int = fuelMin(positions, identity)
-def evaluatorTwo(positions: List[Int]): Int = fuelMin(positions, dist => dist * (dist + 1) / 2)
+def parseInput(input: String) = input.split(",").map(_.toInt).toVector
+
+def evaluatorOne(positions: Vector[Int]): Int = {
+    val crabs = positions.sorted
+    val half = positions.size >> 1
+    
+    val median = 
+        if ((crabs.size & 1) == 1) then crabs(half)
+        else crabs(half - 1).midpoint(crabs(half))
+
+    return crabs.map(n => (n - median).abs).sum
+}
+
+def evaluatorTwo(positions: Vector[Int]): Int = {
+    val mean = positions.sum / positions.size
+
+    def triangle(x: Int, mean: Int): Int = {
+        val n = (x - mean).abs
+        return (n * (n + 1)) / 2
+    }
+
+    return (-1 to 1).map(delta => {
+        positions.map(x => triangle(x, mean + delta)).sum
+    }).min
+}
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
     Using(Source.fromResource(filePath))(_.getLines().toList)
