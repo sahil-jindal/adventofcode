3015f4429bb487bbe3ec5b29a4dc498314080d7f
sahil-jindal <sahiljindal.thebest@gmail.com>
2026-04-05 14:38:46 +0000

    Using maths over simulation

--- 2019/src/main/scala/day10/day10.scala
+++ 2019/src/main/scala/day10/day10.scala
@@ -2,13 +2,16 @@ package day10
 
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
-import scala.collection.mutable.{ArrayBuffer, Queue}
+import scala.util.boundary, boundary.break
 
 case class Vec2D(y: Int, x: Int) {
+    def *(num: Int) = Vec2D(num * y, num * x)
     def -(that: Vec2D) = Vec2D(y - that.y, x - that.x)
+    def +(that: Vec2D) = Vec2D(y + that.y, x + that.x)
 }
 
-case class Input(station: Vec2D, asteroidsByDir: Map[Vec2D, Set[Vec2D]])
+case class Pair(dir: Vec2D, gcd: Int)
+case class Input(station: Vec2D, asteroidsByDir: Map[Vec2D, Set[Int]])
 
 def gcd(a: Int, b: Int): Int = if b == 0 then a else gcd(b, a % b)
 
@@ -20,13 +23,13 @@ def parseInput(input: List[String]): Set[Vec2D] = {
     } yield Vec2D(y, x)).toSet
 }
 
-def getDirection(dir: Vec2D): Vec2D = {
+def getDirection(dir: Vec2D): Pair = {
     val g = gcd(dir.y, dir.x).abs
-    return Vec2D(dir.y / g, dir.x / g)
+    return Pair(Vec2D(dir.y / g, dir.x / g), g)
 }
 
 def groupByDirection(station: Vec2D, remaining: Set[Vec2D]): Input = {
-    return Input(station, remaining.groupBy(it => getDirection(it - station)))
+    return Input(station, remaining.map(it => getDirection(it - station)).groupMap(_.dir)(_.gcd))
 }
 
 def selectStationPosition(asteroids: Set[Vec2D]): Input = {
@@ -38,9 +41,23 @@ def angle(dir: Vec2D): Double = {
     return if (result < 0) { result + 360 } else { result }
 }
 
-def distanceSq(point: Vec2D): Int = {
-    val Vec2D(y, x) = point
-    return x*x + y*y
+def findPosition(numbers: List[Int], position: Int): (Int, Int) = {
+    val length = numbers.max
+    var currPosition = position
+
+    val freq = Array.ofDim[Int](length + 1)
+    numbers.foreach { it => freq(it) += 1 }
+
+    val ringSizes = freq.scanRight(0)(_ + _).slice(1, length + 1)
+
+    boundary {
+        for ((ringSize, i) <- ringSizes.zipWithIndex) {
+            if (currPosition <= ringSize) break((currPosition - 1, i))
+            else { currPosition -= ringSize }
+        }
+
+        (-1, -1)
+    }
 }
 
 def evaluatorOne(input: Input): Int = input.asteroidsByDir.size
@@ -48,29 +65,18 @@ def evaluatorOne(input: Input): Int = input.asteroidsByDir.size
 def evaluatorTwo(input: Input): Int = {
     val Input(station, asteroidsByDir) = input
 
-    val orderedDirection = asteroidsByDir.keySet.toList.sortBy(angle)
+    val dirSizes = asteroidsByDir.view.mapValues(_.size).toMap
+
+    // Using the fact that the problem is based on finite world
+    require(dirSizes.values.sum >= 200)
+
+    val (dirIdx, gcdIdx) = findPosition(dirSizes.values.toList, 200)
+
+    // Great Place to use quickstand here, or a built-in that gives n-th smallest element
+    val dir = dirSizes.collect { case (d, s) if s >= gcdIdx + 1 => d }.toList.sortBy(angle).apply(dirIdx)
+    val gcd = asteroidsByDir(dir).toList.sorted.apply(gcdIdx)
     
-    val asteroids = ArrayBuffer.from(orderedDirection.map(dir => {
-        Queue.from(asteroidsByDir(dir).toList.sortBy(it => distanceSq(it - station)))
-    }))
-     
-    val vaporized = ArrayBuffer.empty[Vec2D]
-
-    while (asteroids.exists(_.nonEmpty)) {
-        var i = 0
-
-        while (i < asteroids.length) {
-            vaporized += asteroids(i).dequeue()
-
-            if (asteroids(i).isEmpty) {
-                asteroids.remove(i).clear()
-            } else {
-                i += 1
-            }
-        }
-    }
-    
-    val Vec2D(y, x) = vaporized(199)
+    val Vec2D(y, x) = station + dir * gcd
     return x * 100 + y
 }
 
