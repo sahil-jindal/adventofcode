ea06ecf8d158eb7c62b644c793590419359555d7
sahil-jindal <sahiljindal.thebest@gmail.com>
2026-04-14 17:14:51 +0000

    Rewrote AOC 2022 Day 4

--- 2022/src/main/scala/day04/day4.scala
+++ 2022/src/main/scala/day04/day4.scala
@@ -2,24 +2,24 @@ package day04
 
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
-import scala.collection.immutable.Range.Inclusive
 
-case class Pair(first: Inclusive, second: Inclusive)
+case class Range(start: Int, end: Int) {
+    def contains(r2: Range) = start <= r2.start && r2.end <= end
+    def overlaps(r2: Range) = r2.start <= end && start <= r2.end
+}
+
+type Pair = (Range, Range)
 
 def parseInput(input: List[String]) = input.map(line => {
     val Seq(sf, ef, ss, es) = raw"(\d+)".r.findAllIn(line).map(_.toInt).toSeq
-    Pair(sf to ef, ss to es)
+    (Range(sf, ef), Range(ss, es))
 })
 
-def duplicateWorkCount(input: List[Pair], rangeCheck: (Inclusive, Inclusive) => Boolean): Int = {
-    return input.count { case Pair(first, second) => rangeCheck(first, second) || rangeCheck(second, first) }
-}
+def partOne(r1: Range, r2: Range) = r1.contains(r2) || r2.contains(r1)
+def partTwo(r1: Range, r2: Range) = r1.overlaps(r2)
 
-def contains(r1: Inclusive, r2: Inclusive): Boolean = r1.start <= r2.start && r2.end <= r1.end 
-def overlaps(r1: Inclusive, r2: Inclusive): Boolean = r2.start <= r1.end && r1.start <= r2.end
-
-def evaluatorOne(input: List[Pair]): Int = duplicateWorkCount(input, contains)
-def evaluatorTwo(input: List[Pair]): Int = duplicateWorkCount(input, overlaps)
+def evaluatorOne(input: List[Pair]): Int = input.count(partOne)
+def evaluatorTwo(input: List[Pair]): Int = input.count(partTwo)
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
     Using(Source.fromResource(filePath))(_.getLines().toList)
