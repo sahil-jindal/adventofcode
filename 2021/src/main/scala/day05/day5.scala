23f55b055b40fd1c3467e957bb51227f88afffbe
sahil-jindal <sahiljindal.thebest@gmail.com>
2026-04-20 10:36:22 +0000

    Rewrote AOC 2021 Day 5

--- 2021/src/main/scala/day05/day5.scala
+++ 2021/src/main/scala/day05/day5.scala
@@ -4,35 +4,40 @@ import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
 
 case class Vec2D(x: Int, y: Int) {
-    def sign = Vec2D(x.sign, y.sign)
     def *(num: Int) = Vec2D(x * num, y * num)
     def +(that: Vec2D) = Vec2D(x + that.x, y + that.y)
     def -(that: Vec2D) = Vec2D(x - that.x, y - that.y)
 }
 
-case class Segment(start: Vec2D, end: Vec2D)
+type Segment = (from: Vec2D, to: Vec2D)
+
+def gcd(a: Int, b: Int): Int = if b == 0 then a else gcd(b, a % b)
 
 def parseInput(input: List[String]) = input.map(line => {
     val Seq(sx, sy, ex, ey) = raw"(\d+)".r.findAllIn(line).map(_.toInt).toSeq
-    Segment(Vec2D(sx, sy), Vec2D(ex, ey))
+    (Vec2D(sx, sy), Vec2D(ex, ey))
 })
 
-def getLines(input: List[Segment], skipDiagonals: Boolean): List[List[Vec2D]] = {
+def getDirection(dir: Vec2D): (Vec2D, Int) = {
+    val Vec2D(y, x) = dir
+    val g = gcd(y.abs, x.abs)
+    return (Vec2D(y / g, x / g), g)
+}
+
+def getPoints(input: List[Segment], skipDiagonals: Boolean): List[Vec2D] = {
     return (for {
-        Segment(start, end) <- input
-        dispmnt = end - start
-        dir = dispmnt.sign
-        length = 1 + math.max(dispmnt.x.abs, dispmnt.y.abs)
+        (from, to) <- input
+        (dir, steps) = getDirection(to - from)
         if !skipDiagonals || dir.x == 0 || dir.y == 0
-    } yield List.tabulate(length)(start + dir * _)).toList
+    } yield List.tabulate(steps + 1)(from + dir * _)).flatten
 }
 
-def getIntersections(lines: List[List[Vec2D]]): Int = {
-    return lines.flatten.groupMapReduce(identity)(_ => 1)(_ + _).values.count(_ > 1)
+def countCommon(points: List[Vec2D]): Int = {
+    return points.groupMapReduce(identity)(_ => 1)(_ + _).values.count(_ > 1)
 }
 
-def evaluatorOne(input: List[Segment]): Int = getIntersections(getLines(input, true))
-def evaluatorTwo(input: List[Segment]): Int = getIntersections(getLines(input, false))
+def evaluatorOne(input: List[Segment]): Int = countCommon(getPoints(input, true))
+def evaluatorTwo(input: List[Segment]): Int = countCommon(getPoints(input, false))
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
     Using(Source.fromResource(filePath))(_.getLines().toList)
