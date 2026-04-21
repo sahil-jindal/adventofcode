545b8b6c1aa7439e0a358344f904946e7a381751
sahil-jindal <sahiljindal.thebest@gmail.com>
2026-04-21 20:01:27 +0000

    Made Mod as Const Type for AOC 2019 day 22

--- 2019/src/main/scala/day22/day22.scala
+++ 2019/src/main/scala/day22/day22.scala
@@ -3,47 +3,48 @@ package day22
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
 
-case class Technique(a: BigInt, c: BigInt, m: BigInt) {
-    infix def compose(other: Technique): Technique = {
-        val m = this.m
+case class Technique[M <: Long](a: BigInt, c: BigInt)(using v: ValueOf[M]) {
+    private val m = BigInt(v.value)
+
+    infix def compose(other: Technique[M]): Technique[M] = {
         val a = (this.a * other.a) % m
         val c = (this.c * other.a + other.c) % m
-        Technique(a, c, m)
+        Technique[M](a, c)
     }
 
-    def inverse(): Technique = {
-        val m = this.m
+    def inverse(): Technique[M] = {
         val a = this.a.modInverse(m)
         val c = m - (a * this.c) % m
-        Technique(a, c, m)
+        Technique[M](a, c)
     }
 
-    def power(e: BigInt): Technique = {
-        val m = this.m
+    def power(e: BigInt): Technique[M] = {
         val a = this.a.modPow(e, m)
         val c = (((a - 1) * (this.a - 1).modInverse(m) % m) * this.c) % m
-        Technique(a, c, m)
+        Technique[M](a, c)
     }
 
     def shuffle(index: BigInt): BigInt = {
-        (this.a * index + this.c) % this.m
+        (this.a * index + this.c) % m
     }
 }
 
-def deck(input: List[String], m: BigInt): Technique = {
+def deck[M <: Long](input: List[String])(using v: ValueOf[M]): Technique[M] = {
+    val m = BigInt(v.value)
+
     val techniques = input.collect {
         case s"cut $num" => {
             val n = BigInt(num)
             val c = (m - n % m) % m
-            Technique(1, c, m)
+            Technique[M](1, c)
         }
         case "deal into new stack" => {
-            Technique(m - 1, m - 1, m)
+            Technique[M](m - 1, m - 1)
         }
         case s"deal with increment $num" => {
             val n = BigInt(num)
             val a = (m + n % m) % m;
-            Technique(a, 0, m)
+            Technique[M](a, 0)
         }
     }
 
@@ -51,11 +52,11 @@ def deck(input: List[String], m: BigInt): Technique = {
 }
 
 def evaluatorOne(input: List[String]): BigInt = {
-    return deck(input, 10007).shuffle(2019)
+    return deck[10007L](input).shuffle(2019)
 }
 
 def evaluatorTwo(input: List[String]): BigInt = {
-    return deck(input, 119315717514047L).inverse().power(101741582076661L).shuffle(2020)
+    return deck[119315717514047L](input).inverse().power(101741582076661L).shuffle(2020)
 }
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
