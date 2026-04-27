7c891e514daaa84f3165a0c23b611be8719ee479
sahil-jindal <sahiljindal.thebest@gmail.com>
2026-04-27 13:36:38 +0000

    Changed Range to Inclusive AOC 2016 Day 20

--- 2016/src/main/scala/day20/day20.scala
+++ 2016/src/main/scala/day20/day20.scala
@@ -3,36 +3,40 @@ package day20
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
 
-case class Range(start: Long, end: Long)
+case class Inclusive(start: Long, end: Long)
 
 def parseInput(input: List[String]) = input.collect {
-    case s"$start-$end" => Range(start.toLong, end.toLong)
+    case s"$start-$end" => Inclusive(start.toLong, end.toLong)
 }
 
-def mergeRanges(ranges: List[Range]): List[Range] = {
+def mergeRanges(ranges: List[Inclusive]): List[Inclusive] = {
     if (ranges.size < 2) return ranges
 
     val sortedRanges = ranges.sortBy(_.start)
     val (current, remaining) = (sortedRanges.head, sortedRanges.tail)
     
     return remaining.foldLeft(List(current)) { (merged, current) =>
-        val Range(lastStart, lastEnd) = merged.last
-        val Range(currStart, currEnd) = current
+        val Inclusive(lastStart, lastEnd) = merged.last
+        val Inclusive(currStart, currEnd) = current
 
         if (currStart <= lastEnd + 1) {
-            merged.init :+ Range(lastStart, lastEnd max currEnd)
+            merged.init :+ Inclusive(lastStart, lastEnd max currEnd)
         } else {
             merged :+ current
         }
     }
 }
 
-def evaluatorOne(ranges: List[Range]): Long = {
-    return (ranges.init zip ranges.tail).collectFirst { case (r1, r2) if r2.start - r1.end > 1 => r1.end + 1 }.get
+def evaluatorOne(ranges: List[Inclusive]): Long = {
+    return (ranges.init zip ranges.tail).collectFirst { 
+        case (r1, r2) if r2.start - r1.end > 1 => r1.end + 1 
+    }.get
 }
 
-def evaluatorTwo(ranges: List[Range]): Long = {
-    return (ranges.init zip ranges.tail).collect { case (r1, r2) if r2.start - r1.end > 1 => r2.start - r1.end - 1 }.sum
+def evaluatorTwo(ranges: List[Inclusive]): Long = {
+    return (ranges.init zip ranges.tail).collect { 
+        case (r1, r2) if r2.start - r1.end > 1 => r2.start - r1.end - 1 
+    }.sum
 }
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
