7353cc05e0a8f7a6037eddcc5b1329cbee45369f
sahil-jindal <sahiljindal.thebest@gmail.com>
2026-04-28 15:04:31 +0000

    Logical Refactor AOC 2025 Day 4

--- 2025/src/main/scala/day04/day4.scala
+++ 2025/src/main/scala/day04/day4.scala
@@ -3,24 +3,7 @@ package day04
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
 
-case class Direction(dy: Int, dx: Int) {
-    def +(that: Direction) = Direction(dy + that.dy, dx + that.dx)
-}
-
-case class Point(y: Int, x: Int) {
-    def +(dir: Direction) = Point(y + dir.dy, x + dir.dx)
-}
-
-val N = Direction(-1, 0)
-val S = Direction(1, 0)
-val E = Direction(0, 1)
-val W = Direction(0, -1)
-val NE = N + E
-val NW = N + W
-val SE = S + E
-val SW = S + W
-
-val directions = List(NW, N, NE, E, SE, S, SW, W)
+case class Point(y: Int, x: Int)
 
 def parseInput(grid: List[String]): Set[Point] = {
     return (for {
@@ -30,8 +13,16 @@ def parseInput(grid: List[String]): Set[Point] = {
     } yield Point(y, x)).toSet
 }
 
+def getNeighbours(pt: Point) = {
+    (for {
+        dy <- -1 to 1
+        dx <- -1 to 1
+        if dy != 0 || dx != 0
+    } yield Point(pt.y + dy, pt.x + dx))
+}
+
 def rollsCondition(points: Set[Point], pt: Point): Boolean = {
-    return directions.map(pt + _).count(points.contains) < 4
+    return getNeighbours(pt).count(points.contains) < 4
 }
 
 def next(points: Set[Point]): Set[Point] = {
