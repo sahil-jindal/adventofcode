6fc13f007adc010619c6092c8b9f941c3259e16a
sahil-jindal <sahiljindal.thebest@gmail.com>
2026-04-29 18:05:33 +0000

    Refactored AOC 2018 Day 2

--- 2018/src/main/scala/day02/day2.scala
+++ 2018/src/main/scala/day02/day2.scala
@@ -3,12 +3,21 @@ package day02
 import scala.util.{Try, Success, Failure, Using}
 import scala.io.Source
 
-def checkLine(input: String, n: Int): Boolean = {
-    return input.groupMapReduce(identity)(_ => 1)(_ + _).values.exists(_ == n)
+extension [A](items: List[A]) {
+    def upperTriangle(): List[(A, A)] = {
+        val partOne = items.tail.tails.toVector
+        return (items zip partOne).init.flatMap {
+            case (e1, ahead) => ahead.map(e1 -> _)
+        }
+    }
+}
+
+def getFrequency(input: String): List[Int] = {
+    return input.groupMapReduce(identity)(_ => 1)(_ + _).values.toList
 }
 
 def diff(line1: String, line2: String): Int = {
-    return (line1 zip line2).count { case (a, b) => a != b }
+    return (line1 zip line2).count(_ != _)
 }
 
 def common(line1: String, line2: String): String = {
@@ -16,20 +25,22 @@ def common(line1: String, line2: String): String = {
 }
 
 def evaluatorOne(input: List[String]): Int = {
-    val doubles = input.count(checkLine(_, 2))
-    val triples = input.count(checkLine(_, 3))
+    val freqlist = input.map(getFrequency)
+    val doubles = freqlist.count(_.exists(_ == 2))
+    val triples = freqlist.count(_.exists(_ == 3))
     return doubles * triples
 }
 
 def evaluatorTwo(input: List[String]): String = {
-    return input.combinations(2).collectFirst {
-        case List(a, b) if diff(a, b) == 1 => common(a, b)
+    return input.upperTriangle().collectFirst {
+        case (a, b) if diff(a, b) == 1 => common(a, b)
     }.get
 }
 
 def readLinesFromFile(filePath: String): Try[List[String]] =
     Using(Source.fromResource(filePath))(_.getLines().toList)
 
+@main
 def hello(): Unit = {
     readLinesFromFile("day02.txt") match {
         case Success(lines) => {
